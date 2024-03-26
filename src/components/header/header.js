4cc09484696aa222b8ab4874139e95de2b9c0204
jaxongir <jaxongirtoshpolatov@mail.ru>
2024-03-26 21:06:11 +0500

    small changes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,21 +1,35 @@
+import { useState } from 'react'
 import './header.css'
 
 function Header() {
+
+    const [status, setStatus] = useState(false);
+
+    const humburger = () => {
+        if(status){
+            setStatus(false)
+        }else{
+            setStatus(true)
+        }
+    }
+
   return (
     <div className="container header__container d-flex">
         {/* brand logo */}
-        <a href="#!" className="brand__logo">Sangardak sanatoriya</a>
+        <a href="#!" className='brand__logo'>Sangardak sanatoriya</a>
         <nav className='nav d-flex'>
-            <ul className="nav__menu d-flex">
+            <ul className={status ? `nav__menu d-flex visible` : `nav__menu d-flex`}>
                 <li className="nav__item"><a href="#!" className="nav__link">Bosh sahifa</a></li>
                 <li className="nav__item"><a href="#!" className="nav__link">Tariff</a></li>
                 <li className="nav__item"><a href="#!" className="nav__link">Xizmatlar</a></li>
                 <li className="nav__item"><a href="#!" className="nav__link">Biz haqimizda</a></li>
                 <li className="nav__item"><a href="#!" className="nav__link">Rahbariyat</a></li>
             </ul>
-            <div className="hamburger__menu d-flex close">
-                <div className="bars__icon"></div>
-            </div>
+                <div className={status ? `bars__icon d-flex active` : `bars__icon d-flex`} onClick={humburger}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
         </nav>
     </div>
   )
