7812535368461986d1efcb9065a990272d0f3184
jaxongir <jaxongirtoshpolatov@mail.ru>
2024-03-26 21:49:49 +0500

    header section finished

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="wrapper">
-      <header className="header">
+      <header className="header" id='header'>
         <Header/>
       </header>
       <main className="main">
