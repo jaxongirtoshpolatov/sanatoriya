950cf0f9e20f8e7e365615476b60334148aad3ff
jaxongir <jaxongirtoshpolatov@mail.ru>
2024-03-26 22:13:34 +0500

    header section finished

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -13,6 +13,10 @@ function Header() {
         }
     }
 
+    const closed = () => {
+        setStatus(false)
+    }
+
 
   return (
     <div className="container header__container d-flex">
@@ -20,11 +24,11 @@ function Header() {
         <a href="#!" className='brand__logo'>Sangardak sanatoriya</a>
         <nav className='nav d-flex'>
             <ul className={status ? `nav__menu d-flex visible` : `nav__menu d-flex`}>
-                <li className="nav__item"><a href="#header" className="nav__link">Bosh sahifa</a></li>
-                <li className="nav__item"><a href="#!" className="nav__link">Tariff</a></li>
-                <li className="nav__item"><a href="#!" className="nav__link">Xizmatlar</a></li>
-                <li className="nav__item"><a href="#!" className="nav__link">Biz haqimizda</a></li>
-                <li className="nav__item"><a href="#!" className="nav__link">Rahbariyat</a></li>
+                <li className="nav__item"><a href="#header" className="nav__link" onClick={closed}>Bosh sahifa</a></li>
+                <li className="nav__item"><a href="#!" className="nav__link" onClick={closed}>Tariff</a></li>
+                <li className="nav__item"><a href="#!" className="nav__link" onClick={closed}>Xizmatlar</a></li>
+                <li className="nav__item"><a href="#!" className="nav__link" onClick={closed}>Biz haqimizda</a></li>
+                <li className="nav__item"><a href="#!" className="nav__link" onClick={closed}>Rahbariyat</a></li>
             </ul>
                 <div className={status ? `bars__icon d-flex active` : `bars__icon d-flex`} onClick={humburger}>
                     <span></span>
