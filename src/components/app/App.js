a5b2784169ecc955adf1625e7b09744b30477b21
jaxongir <jaxongirtoshpolatov@mail.ru>
2024-03-27 23:18:37 +0500

    header section finished

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,6 +3,10 @@ import Intro from '../intro/intro';
 import './App.css';
 
 function App() {
+  const slides = [
+    { id: 1, image: 'https://shorturl.at/hkHQX', title: 'Yangiliklar Sarlavhasi 1', descr: 'Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, ' },
+    { id: 2, image: 'https://shorturl.at/ejrSW', title: 'Yangiliklar sarlavhasi 2', descr: 'Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, ' },
+  ];
   return (
     <div className="wrapper">
       <header className="header" id='header'>
@@ -10,7 +14,7 @@ function App() {
       </header>
       <main className="main">
         <section className='intro'>
-          <Intro/>
+          <Intro slides={slides}/>
         </section>
       </main>
       <footer className='footer'>
--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,9 +1,57 @@
-import './intro.css'
+import "./intro.css";
+import next from "./images/right.svg";
+import { useState, useEffect } from "react";
 
-function Intro() {
+function Intro({ slides }) {
+  const [count, setCount] = useState(0);
+
+  const nextSlide = () => {
+    setCount((prevCount) => (prevCount + 1) % slides.length);
+  };
+
+  const prevSlide = () => {
+    setCount((prevCount) => (prevCount - 1 + slides.length) % slides.length);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      nextSlide();
+    }, 5000); // 5 sekunddan so'ng keyingi slaydga o'tish
+    return () => clearTimeout(timer); // Komponent unmont bo'lganda timeout ni to'xtatish
+  }, [count]);
+
   return (
-    <div className="container intro__container"></div>
-  )
+    <div className="slider__wrapper">
+      {slides.map((item, index) => {
+        return (
+          <div
+            className={count === index ? "slider fade-in " : "slider hidden"}
+            style={{ backgroundImage: `url(${item.image})` }}
+          >
+            <div className="slide" key={item.id}>
+              <h1 className="slide__title">{item.title}</h1>
+              <p className="slide__descr">{item.descr}</p>
+              <button className="slide__more_btn">Batafsil</button>
+            </div>
+            <div className="slide__btns">
+              <img
+                src={next}
+                alt="next icon"
+                className="prev__icon arrow__icon"
+                onClick={prevSlide}
+              />
+              <img
+                src={next}
+                alt="next icon"
+                className="next__icon arrow__icon"
+                onClick={nextSlide}
+              />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default Intro
+export default Intro;
